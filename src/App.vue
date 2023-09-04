ccfd6cd5d70885b13bf7798b87397046b00c8ea1
Mario Santoro <126876743+MarioSantoro@users.noreply.github.com>
2023-09-04 18:14:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    Header
+  }
+}
+
+
+</script>
+
+<template>
+  <Header />
+  <router-view>
+  </router-view>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@import "bootstrap/scss/bootstrap";
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,52 @@
+<template>
+    <article>
+        <div class="container-fluid wrapper">
+            <div class="row justify-content-center py-4">
+                <SingleCocktail v-for="cocktail in cocktails" :cocktail="cocktail" />
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+import SingleCocktail from "../components/SingleCocktail.vue";
+import  axios  from "axios";
+export default {
+    data() {
+        return {
+            apiUrl: 'http://127.0.0.1:8000/api/cocktails',
+            cocktails: [],
+        }
+    },
+    methods: {
+        getData() {
+            axios.get(this.apiUrl)
+                .then( (response) => {
+                    this.cocktails = response.data.data;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+    },
+    created() {
+        this.getData();
+    },
+    components: {
+        SingleCocktail
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+article {
+    /* Created with https://www.css-gradient.com */
+    background: #161616;
+}
+
+.wrapper {
+    max-width: 1500px;
+    margin: 0 auto;
+}
+</style>
